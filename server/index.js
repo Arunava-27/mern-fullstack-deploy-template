1a6cc23990632b06a6ca2fb10989a5390cd38c97
Arunava-27 <drarunkundu22@gmail.com>
2025-11-08 17:48:42 +0530

    Refactor API fetch URL and configure Vite server proxy for API requests

--- server/index.js
+++ server/index.js
@@ -1,17 +1,30 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-const PORT=5000
+const PORT = 5000;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:3000",
+    //   add production url
+    ],
+    credentials: true,
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
 
 //API route
 app.get("/api/message", (req, res) => {
-    res.json({message: "Hello from arunava server"})
-    console.log("Hello");
-    
-})
+  res.json({ message: "Hello from arunava server" });
+  console.log("Hello");
+});
 
-app.listen(PORT, ()=> console.log(`Server is running at http://localhost:${PORT}`))
+app.listen(PORT, () =>
+  console.log(`Server is running at http://localhost:${PORT}`)
+);
